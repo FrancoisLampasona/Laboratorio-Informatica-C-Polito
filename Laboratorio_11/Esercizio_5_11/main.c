cf22da212eb20b4e1d60f945de619aacab81a85e
zphooenix <lampasonafrancois@gmail.com>
2022-07-10 14:32:41 +0200

    modifiche

--- Laboratorio_11/Esercizio_5_11/main.c
+++ Laboratorio_11/Esercizio_5_11/main.c
@@ -3,12 +3,60 @@
 //
 
 #include <stdio.h>
-#inlcude <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 100
+
+typedef struct list {
+char nome[20];
+int punteggio;
+}list;
 
 
-int main (void){
+int main (int argc , const char *argv[]){
 
-    printf("Ciao");
+    if (argc!=2){
+        printf("Errore passaggio parametri\n");
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1],"r");
+
+    list tabella[N];
+    int i = 0;
+
+
+    while (!feof(fp))
+    {
+           fscanf(fp,"%s %d",tabella[i].nome,&tabella[i].punteggio);
+             i++;
+            
+    }
+
+    fclose(fp);
+    int k = 0;
+
+
+     while (k!=i)
+
+        {
+            for (int j = 1; j < i-1; j++)
+            {
+                
+            if (strcmp(tabella[k].nome,tabella[i].nome)>0)
+            
+            {
+                printf("%s %d",tabella[k].nome,tabella[k].punteggio);
+            }
+
+            }
+            
+           
+            
+        }
+   
+    
+    
 
     
 }
